263a48cb330cb2722620d5be37f32c79917e6dc1
Emanuel Fagundes <emanuelfafa1313@gmail.com>
2023-09-21 11:48:16 -0400

    Adicionar arquivos de projeto.

---
+++ SimuladorBombaCombustivel/BombaDeCombustivel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimuladorBombaCombustivel
+{
+    public class BombaDeCombustivel
+    {
+        private string tipoCombustivel;
+        private double valorLitro;
+        private double quantidadeCombustivel;
+
+        public BombaDeCombustivel(string tipoCombustivel, double valorLitro, double quantidadeCombustivel)
+        {
+            this.tipoCombustivel = tipoCombustivel;
+            this.valorLitro = valorLitro;
+            this.quantidadeCombustivel = quantidadeCombustivel;
+        }
+
+        public string TipoCombustivel
+        {
+            get
+            {
+                return tipoCombustivel;
+            }
+
+        }
+        public double ValorLitro
+        {
+            get
+            {
+                return valorLitro;
+            }
+
+        }
+        public double QuantidadeCombustivel
+        {
+            get
+            {
+                return quantidadeCombustivel;
+            }
+
+        }
+
+        public double AbastecerPorValor(double valorAbast)
+        {
+            double quantLitros = valorAbast / ValorLitro;
+
+            if (valorAbast <= QuantidadeCombustivel)
+            { 
+                quantidadeCombustivel -= quantLitros;               
+            }
+            return quantLitros;
+        }
+        public double AbastecerPorLitro(double quantLitros)
+        {
+            double valorASerPago = quantLitros * ValorLitro;
+
+            if (quantLitros <= QuantidadeCombustivel)
+            {
+                quantidadeCombustivel -= quantLitros;                
+            }
+            return valorASerPago;
+
+        }
+        public void AlterarValor(double nvValor)
+        {
+            valorLitro = nvValor;
+        }
+        public void AlterarCombustivel(string nvTipo)
+        {
+            tipoCombustivel = nvTipo;
+        }
+        public void AlterarQuantidadeDeCombustivel(double nvQuant)
+        {
+            quantidadeCombustivel = nvQuant;
+        }
+        public void MostarInformacoesBomba()
+        {
+            Console.WriteLine($"\n\t\t---Informações Bomba---\n" +
+                $"======================================================\n" +
+                $"Tipo de Combustivel\t\t\t{TipoCombustivel}\n" +
+                $"Valor por Litro\t\t\t\t{ValorLitro:c2}\n" +
+                $"Quantidade de Combustivel restante\t{QuantidadeCombustivel} litros\n" +
+                $"======================================================\n");
+        }
+    }
+}
---
+++ SimuladorBombaCombustivel/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimuladorBombaCombustivel
+{
+    internal class Program
+    {
+        static BombaDeCombustivel bomba1 = new BombaDeCombustivel("Gasolina", 3, 1000);
+        static void Main(string[] args)
+        {
+            // Emanuel e Henrique 2A informatica
+
+            int opc;
+            do
+            {
+                opc = 0;
+                Console.WriteLine("\t\t---Simulador Bomba Combustivel---\n\n");
+                bomba1.MostarInformacoesBomba();
+                Console.Write("\n[1] Abastecer por Litro\n" +
+                    "[2] Abastecer por valor\n" +
+                    "[3] Alterar valor de combustivel\n" +
+                    "[4] ALterar tipo combustivel\n" +
+                    "[5] Alterar quantidade de combustivel\n" +
+                    "[6] Sair\n" +
+                    "\nQual opção deseja execultar: ");
+                opc = int.Parse(Console.ReadLine());
+
+                if (opc > 0)
+                {
+                    switch (opc)
+                    {
+                        case 1:
+                            Console.Write("Informe a quantidade que deseja abastecer: ");
+                            double Litro = double.Parse(Console.ReadLine());
+                            if (Litro > 0)
+                            {
+                                if (Litro <= bomba1.QuantidadeCombustivel)
+                                {
+                                    bomba1.AbastecerPorLitro(Litro);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("A quantidade que deseja abastecer exede a quantidade disponivel na bomba!");
+                                    Console.ReadKey();
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("O valor informado é negativo!");
+                                Console.ReadKey();
+                            }
+                            break;
+
+                        case 2:
+                            Console.Write("Informe o valor que deseja abastecer: ");
+                            double valorAbastec = double.Parse(Console.ReadLine());
+                            if (valorAbastec > 0)
+                            {
+                                if (valorAbastec / bomba1.ValorLitro <= bomba1.QuantidadeCombustivel)
+                                {
+                                    bomba1.AbastecerPorValor(valorAbastec);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("A quantidade que deseja abastecer exede a quantidade disponivel na bomba!");
+                                    Console.ReadKey();
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("O valor informado é negativo!");
+                                Console.ReadKey();
+                            }
+                            
+                            break;
+
+                        case 3:
+                            Console.Write("Informe o novo valor do combustivel: ");
+                            double nvValor = double.Parse(Console.ReadLine());
+                            if (nvValor > 0)
+                            {
+                                bomba1.AlterarValor(nvValor);
+                            }
+                            else
+                            {
+                                Console.WriteLine("O valor informado é negativo!");
+                                Console.ReadKey();
+                            }
+                            break;
+
+                        case 4:
+                            Console.Write("Informe o novo tipo de combustivel: ");
+                            string nvTipo = Console.ReadLine();
+                            bomba1.AlterarCombustivel(nvTipo);
+                            break;
+
+                        case 5:
+                            Console.Write("Informe a nova quantidade de combustivel que a maquina possuirá: ");
+                            double nvQuantidade = double.Parse(Console.ReadLine());
+                            if (nvQuantidade > 0)
+                            {
+                                bomba1.AlterarQuantidadeDeCombustivel(nvQuantidade);
+                            }
+                            else
+                            {
+                                Console.WriteLine("O valor informado é negativo!");
+                                Console.ReadKey();
+                            }
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Opção é menor que zero!");
+                    Console.ReadKey();
+                }
+                Console.Clear();
+            } while (opc != 6);
+
+            Console.WriteLine("Programa finalizado!");
+            Console.ReadKey();
+        }
+    }
+}
